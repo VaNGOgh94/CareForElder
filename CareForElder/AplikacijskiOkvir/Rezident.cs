5c3a7361b2730642fca97c08dd5e91f80731be86
tomcivcij <tomcivcij@foi.hr>
2016-08-23 17:24:33 +0200

    Nova klasa

---
+++ CareForElder/AplikacijskiOkvir/Rezident.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikacijskiOkvir {
+	class Rezident : Osoba{
+		private DateTime datumUseljenja;
+		private DateTime datumIseljenja;
+		List<string> listaZahtjeva;
+
+	}
+}
